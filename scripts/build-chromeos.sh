0353e33b8532b0cd6adb3fe3693bc6fac544fe2d
Koji Ishii <kojii@chromium.org>
2021-05-01 03:09:49 +0900

    Update references for ChromeOS NotoSansCJK 2.004
    Moved reference files from "`references/chromeos`" to
    "`references`" as the version of the fonts in ChromeOS
    now matches with the latest Google Fonts release.

--- scripts/build-chromeos.sh
+++ scripts/build-chromeos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 export LOG_NAME=chromeos.log
-export REF_NAME=chromeos
+# export REF_NAME=chromeos
 "$SCRIPT_DIR/build-noto-cjk.sh" $*
