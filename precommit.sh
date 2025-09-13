8bfaac15188b957049c784d3ce90aab7b9156f6d
Koji Ishii <kojii@chromium.org>
2025-09-14 06:06:00 +0900

    Remove the pytype dev dependency (#312)
    This is a speculative fix for the dependabot errors #298.
    
    The `python_version <= '3.12'` for the [pytype] dependency is suspicious, but [pytype] has no plans to support Python 3.13.
    
    [pytype]: https://github.com/google/pytype/

--- precommit.sh
+++ precommit.sh
@@ -10,4 +10,7 @@ set -e
 yapf -ir -vv .
 pytest -v
 # tox -p
-pytype src/east_asian_spacing
+
+# `pytype` supports 3.8-3.12 (May 15, 2025)
+# https://github.com/google/pytype/#requirements
+uvx --python 3.12 --with fonttools,uharfbuzz pytype src/east_asian_spacing
